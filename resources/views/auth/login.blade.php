d06ac47a4b1fe5bf89905ffb0e65803e1fd7d407
澤田大樹 <hiroki.bigtree@icloud.com>
2021-02-27 23:31:00 +0900

    エラー解消

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <h2 class="title">kanbanにログイン</h2>
             <div class="text-center m-3">or</div>
-                <div class="tect-center">
+                <div class="text-center">
                     <p class="acountPage_link"><a href="{{ route('register') }}">アカウントを作成</a></p>
             </div>
         <form class="new_user" id="new_user" action="{{ route('login') }}" accept-charset="UTF-8" method="post">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,18 +11,18 @@
     <title>Kanban</title>
 
     <!-- Styles(ローカルの場合、secure_asset=>assetにすること) -->
-    <link href="{{ secure_asset('css/sign_up.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/sign_in.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/app.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/top.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/button.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/common.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/header.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/card_new.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/card_show.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/card_edit.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/list_new.css'} })" rel="stylesheet">
-    <link href="{{ secure_asset('css/list_edit.css'} })" rel="stylesheet">
+    <link href="{{ asset('css/sign_up.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sign_in.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/top.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/button.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/card_new.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/card_show.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/card_edit.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/list_new.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/list_edit.css') }}" rel="stylesheet">
 
     <!-- awesome fonts -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
